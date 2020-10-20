0a01139bb47664748361c0c75f1fbb5e349935d7
Maria <mvvu@stud.ntnu.no>
2020-10-20 22:11:06 +0200

    Fixed state of radio buttons

--- src/routes/AlreadyVoted/index.tsx
+++ src/routes/AlreadyVoted/index.tsx
@@ -3,14 +3,13 @@ import { RadioGroup, FormControl, FormControlLabel, Radio, Checkbox, CheckboxPro
 import './index.css';
 import { useHistory } from 'react-router';
 
-//Bruk useForm i stedet, sjekk loginform i components
 
 const AlreadyVoted = () => {
-    const [value, setValue] = useState("infavor");
+    const [value, setValue] = useState("");
     const [isChecked, setIsChecked] = useState(false);
 
     const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
-        (e.currentTarget.value === value)? setValue(""): setValue(e.currentTarget.value)
+        setValue(e.currentTarget.value);
     }
 
     const history = useHistory();
@@ -18,8 +17,6 @@ const AlreadyVoted = () => {
         history.push('/vote');
     }
 
-    console.log(isChecked);
-    //console.log(value);
     return(
         <div className="already-voted-page">
             <h2 className="already-voted-page_title">THANK YOU FOR VOTING</h2>
@@ -27,10 +24,10 @@ const AlreadyVoted = () => {
                 <div className="already-voted-page_paragraph">
                     <h3>Would you like to let TenBond know what you voted?</h3>
                     <FormControl component='fieldset'>
-                        <RadioGroup aria-label="vote" name="vote1" value={value}>
-                            <FormControlLabel value="infavor" control={<Radio color="primary" onChange={handleChange}/>} label="I voted in favor of the proposed resolution" />
-                            <FormControlLabel value="disfavor" control={<Radio color="primary" onChange={handleChange}/>} label="I voted in disfavor of the proposed resolution" />
-                            <FormControlLabel value="disclose" control={<Radio color="primary" onChange={handleChange}/>} label="I do not want to disclose" />
+                        <RadioGroup aria-label="vote" name="vote1" value={value} onChange={handleChange} className="already-voted-page_radio">
+                            <FormControlLabel value="infavor" control={<Radio color="primary" />} label="I voted in favor of the proposed resolution" />
+                            <FormControlLabel value="disfavor" control={<Radio color="primary" />} label="I voted in disfavor of the proposed resolution" />
+                            <FormControlLabel value="disclose" control={<Radio color="primary" />} label="I do not want to disclose" />
                         </RadioGroup>
                     </FormControl>
                 </div>
