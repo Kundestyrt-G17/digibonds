e366e8ef088b934fd9b2c1490f351394b1b49122
Maria <mvvu@stud.ntnu.no>
2020-10-22 13:26:13 +0200

    button disabled when checkbox not checked

--- src/routes/AlreadyVoted/index.tsx
+++ src/routes/AlreadyVoted/index.tsx
@@ -30,6 +30,7 @@ const AlreadyVoted = (props: Props) => {
   const { setSubmitted } = props;
   const [value, setValue] = useState('');
   const [isChecked, setIsChecked] = useState(false);
+  const disabled = (value==='');
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
     setValue(e.currentTarget.value);
@@ -67,11 +68,6 @@ const AlreadyVoted = (props: Props) => {
                 control={<Radio color="primary" />}
                 label="I voted in disfavor of the proposed resolution"
               />
-              <FormControlLabel
-                value="disclose"
-                control={<Radio color="primary" />}
-                label="I do not want to disclose"
-              />
             </RadioGroup>
           </FormControl>
         </div>
@@ -87,7 +83,7 @@ const AlreadyVoted = (props: Props) => {
           label="The company allows their responsible broker to get insight in voting choice"
         />
         <div className="already-voted-page_button">
-          <Button variant="contained" color="primary" onClick={handleVoteNow}>
+          <Button variant="contained" color="primary" onClick={handleVoteNow} disabled={!isChecked || disabled}>
             Let us know
           </Button>
         </div>
