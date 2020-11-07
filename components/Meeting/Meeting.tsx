1403808582c18b6f347be36fe6c8bc97768ad2b9
Pernille Welle-Watne <perniww@online.no>
2020-11-07 16:02:09 +0100

    make sort and filter and fix styling pfor meeting name

--- components/Meeting/Meeting.tsx
+++ components/Meeting/Meeting.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import InvestorTable from "@/components/InvestorTable/InvestorTable";
+import InvestorTable from "@/components/Meeting/InvestorTable/InvestorTable";
 import useSWR from "swr";
 import { useRouter } from "next/router";
 import styles from "./Meetings.module.css";
-import { InputAdornment, TextField } from "@material-ui/core";
-import SearchIcon from "@material-ui/icons/Search";
+import { Button } from "@material-ui/core";
 import { IMeeting } from "@/schemas/meeting";
+import SearchFilter from "@/components/Meeting/SearchFilter";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -13,42 +13,71 @@ const Meeting = () => {
   const router = useRouter();
   const { data, error } = useSWR<IMeeting>(`/api${router.asPath}`, fetcher);
   const [search, setSearch] = useState("");
+  const [checkboxStates, setCheckboxStates] = useState<{
+    voted: boolean;
+    poh: boolean;
+  }>({
+    voted: false,
+    poh: false,
+  });
 
   if (error) return <div>Failed to Load</div>;
   if (!data) return <div>Loading...</div>;
 
-  const filteredVotes = data.votes.filter((d) =>
+  const filterVotes = data.votes.filter((vote) => {
+    if (checkboxStates.poh) {
+      return vote.pohStatus === "Approved";
+    }
+    if (checkboxStates.voted) {
+      return vote.favor !== "Not voted";
+    }
+    if (checkboxStates.voted && checkboxStates.poh) {
+      return vote.favor !== "Not voted" && vote.pohStatus === "Approved";
+    }
+    return vote;
+  });
+
+  console.log(filterVotes);
+
+  const searchVotes = filterVotes.filter((d) =>
     d.company.name.toLowerCase().includes(search.toLowerCase())
   );
 
+  const date = new Date(data?.date);
+
   return (
     <div>
+      <Button href={"/"}>Back</Button>
       {error && (
         <p className={styles.errorMessage}>
           An error has occurred. Please contact the IT department.{" "}
-          {error.message}{" "}
+          {error.message}
         </p>
       )}
-      <h4>Bondholder meeting for </h4>
-      <h1>{data?.meetingName}</h1>
-      <TextField
-        label="Search"
-        variant="outlined"
-        margin="normal"
-        type="search"
-        name="search"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        style={{ height: "50px", margin: "0" }}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <SearchIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
-      <InvestorTable votes={filteredVotes} totalBonds={data.totalBonds} />
+      <div>
+        <h1 style={{ fontSize: "24px" }}>Bondholder meeting for </h1>
+        <div style={{ display: "flex", marginBottom: "36px" }}>
+          <h2 style={{ fontSize: "60px", margin: "0" }}>{data?.meetingName}</h2>
+          <h3
+            style={{
+              alignSelf: "flex-end",
+              marginLeft: "36px",
+              fontSize: "12px",
+              color: "#737B81",
+            }}
+          >
+            {`${date.getDay()}.${date.getDate()}.${date.getFullYear()}`}
+          </h3>
+        </div>
+      </div>
+      <div>
+        <SearchFilter
+          setSearch={setSearch}
+          checked={checkboxStates}
+          setCheckedStates={setCheckboxStates}
+        />
+      </div>
+      <InvestorTable votes={searchVotes} totalBonds={data.totalBonds} />
     </div>
   );
 };
---
+++ components/Meeting/SearchFilter.tsx
@@ -0,0 +1,143 @@
+import React, { ChangeEvent, useState } from "react";
+import {
+  Button,
+  Checkbox,
+  ClickAwayListener,
+  createStyles,
+  Divider,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  IconButton,
+  InputBase,
+  Paper,
+  Theme,
+} from "@material-ui/core";
+import FilterListIcon from "@material-ui/icons/FilterList";
+import SearchIcon from "@material-ui/icons/Search";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      margin: "10px 0px",
+      padding: "2px 4px",
+      display: "flex",
+      alignItems: "center",
+      width: 400,
+    },
+    input: {
+      flex: 1,
+      marginLeft: "4px",
+    },
+    iconButton: {
+      padding: 10,
+    },
+    divider: {
+      height: 28,
+      margin: 4,
+    },
+  })
+);
+
+interface SearchFilterProps {
+  setSearch: (e) => void;
+  checked: { voted: boolean; poh: boolean };
+  setCheckedStates: ({ voted, poh }) => void;
+}
+
+export default function SearchFilter(props: SearchFilterProps) {
+  const { setSearch, checked, setCheckedStates } = props;
+
+  const [showFilterMenu, setShowFilterMenu] = useState<boolean>();
+
+  const classes = useStyles();
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setCheckedStates({ ...checked, [event.target.name]: event.target.checked });
+  }
+
+  return (
+    <div style={{ position: "relative" }}>
+      <Paper component="form" className={classes.root}>
+        <InputBase
+          className={classes.input}
+          placeholder="Search for company"
+          inputProps={{ "aria-label": "search" }}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <IconButton
+          type="submit"
+          className={classes.iconButton}
+          aria-label="search"
+        >
+          <SearchIcon />
+        </IconButton>
+        <Divider className={classes.divider} orientation="vertical" />
+        <Button
+          className={classes.iconButton}
+          onClick={() => setShowFilterMenu(true)}
+          startIcon={
+            <FilterListIcon
+              style={
+                checked.voted || checked.poh
+                  ? { fill: "blue" }
+                  : { fill: "grey" }
+              }
+            />
+          }
+        >
+          Filter result
+        </Button>
+      </Paper>
+      {showFilterMenu && (
+        <ClickAwayListener
+          onClickAway={() => {
+            setShowFilterMenu(false);
+          }}
+        >
+          <Paper
+            style={{
+              backgroundColor: "#F1F4FF",
+              zIndex: "10",
+              position: "absolute",
+              top: "-108px",
+              left: "270px",
+              padding: "10px",
+            }}
+          >
+            <FormControl component="fieldset">
+              <FormLabel component="legend">Filter options</FormLabel>
+              <FormGroup color="primary">
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={checked.voted}
+                      onChange={handleChange}
+                      name="voted"
+                    />
+                  }
+                  label="Voted"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={checked.poh}
+                      onChange={handleChange}
+                      name="poh"
+                    />
+                  }
+                  label="Valid Proof of holding"
+                />
+                <Button onClick={() => setShowFilterMenu(false)}>
+                  Apply options
+                </Button>
+              </FormGroup>
+            </FormControl>
+          </Paper>
+        </ClickAwayListener>
+      )}
+    </div>
+  );
+}
